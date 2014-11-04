6d3a945b2cfef796829aae2e349f410ea844a9bb
Xie Haozhe <zjhzxhz@gmail.com>
2014-11-04 12:17:41 +0800

    Replace function name to avoid conflict with other plugins according to WordPress's restrictions.

--- google-font-fix.php
+++ google-font-fix.php
@@ -13,15 +13,15 @@ function google_apis_fix($buffer) {
 	return str_replace('googleapis.com', 'useso.com', $buffer);
 }
 
-function string_buffer_start() {
+function gpf_buffer_start() {
 	ob_start("google_apis_fix");
 }
 
-function string_buffer_end() {
+function gpf_buffer_end() {
 	ob_end_flush();
 }
 
-add_action('init', 'string_buffer_start');
-add_action('shutdown', 'string_buffer_end');
+add_action('init', 'gpf_buffer_start');
+add_action('shutdown', 'gpf_buffer_end');
 
 ?>
